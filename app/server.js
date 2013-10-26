ffa8e731e7722cf3e4603063d52b578b9172b272
Ross Penman <ross@pen.mn>
2013-10-27 00:03:54 +0100

    Added show episodes

--- app/server.js
+++ app/server.js
@@ -1,4 +1,5 @@
 GLOBAL.config = require('nconf').file({ file: 'config.json' })
+console.log(config.get('port'))
 
 var models = require('./models')
 
@@ -8,8 +9,16 @@ var Shownotes = models.shownotes
 var express = require('express')
 var app = express()
 
+Episode.create({ title: "hello", ytURL: "", published: true, approved: false })
+
 app.get('/', function(req, res){
 
+})
+
+app.get('/:id', function(req, res) {
+  Episode.find(parseInt(req.param('id'), 10)).success(function(episodes) {
+    // this gets the episode
+  })
 })
 
 app.listen(config.get('port') || 3000)
