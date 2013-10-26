088a1418a7ddaff809152ac39a39e93ab0ca2dc3
Will Smidlein <Will@WillSmidlein.com>
2013-10-26 17:18:36 -0400

    Models and apps and things, oh my

--- app/models/index.js
+++ app/models/index.js
@@ -1,9 +1,14 @@
 var Sequelize = require('sequelize')
 
-var sequelize = new Sequelize(config.get('name'), config.get('user'), config.get('password'))
+var dbConfig = config.get('db')
+
+var sequelize = new Sequelize(dbConfig.name, dbConfig.user, dbConfig.password)
 
 module.exports['episode'] = sequelize.import(__dirname + '/episode.js')
 module.exports['shownotes'] = sequelize.import(__dirname + '/shownotes.js')
+
+module.exports['episode'].sync()
+module.exports['shownotes'].sync()
 
 module.exports['episode'].hasMany(module.exports['shownotes'], {as: 'shownotes_translations'})
 
--- app/server.js
+++ app/server.js
@@ -1,1 +1,15 @@
 GLOBAL.config = require('nconf').file({ file: 'config.json' })
+
+var models = require('./models')
+
+var Episode = models.episode
+var Shownotes = models.shownotes
+
+var express = require('express')
+var app = express()
+
+app.get('/', function(req, res){
+
+})
+
+app.listen(config.get('port') || 3000)
