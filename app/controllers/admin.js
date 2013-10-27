ab6c1cdc7fe81fd8a49745b97bdb11aa43d229a4
Charley Hutchison <glenwayguy@gmail.com>
2013-10-27 14:34:47 -0500

    Added moe things to controllers

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -2,89 +2,89 @@
 
 module.exports.get = function(req, res) {
 	var data = {
-		boxes: [
-			{
-				title: "Video views today",
-				data: 0
-			},
-			{
-				title: "Videos awaiting approval",
-				data: 0
-			},
-			{
-				title: "Transcriptions awaiting approval",
-				data: 0
-			},
-			{
-				title: "Some title here",
-				data: "Data"
-			},
-			{
-				title: "Some title here",
-				data: "Data"
-			},
-			{
-				title: "Some title here",
-				data: "Data"
-			}
-		]
-	}
-	async.parallel([
-		function(callback) { // Total views
-			callback(null, '12,428')
-		}, 
-		function(callback) { // Videos awaiting approval
-			sequelize.query('SELECT * FROM Episodes WHERE approved = 0').success(function(query) {
-				var grammar = query.length === 1 ?
-							  'Video awaiting approval' :
-							  'Videos awaiting approval'
-				callback(null, [grammar, query.length])
-			})
-		},
-		function(callback) {
-			callback(null, 15)
+			boxes: [
+				{
+					title: "Video views today",
+					data: 0
+				},
+				{
+					title: "Videos awaiting approval",
+					data: 0
+				},
+				{
+					title: "Transcriptions awaiting approval",
+					data: 0
+				},
+				{
+					title: "Some title here",
+					data: "Data"
+				},
+				{
+					title: "Some title here",
+					data: "Data"
+				},
+				{
+					title: "Some title here",
+					data: "Data"
+				}
+			]
 		}
-	],
-	function(err, callback) {
-		for (var i in callback) {
-			if (typeof(callback[i]) === 'object') { // Grammar easter egg
-				data['boxes'][i].title = callback[i][0]
-				data['boxes'][i].data = callback[i][1]
-			} else {
-				data['boxes'][i].data = callback[i]
-				if (i == callback.length - 1) res.render('admin/admin', data)				
+		async.parallel([
+			function(callback) { // Total views
+				callback(null, '12,428')
+			}, 
+			function(callback) { // Videos awaiting approval
+				sequelize.query('SELECT * FROM Episodes WHERE approved = 0').success(function(query) {
+					var grammar = query.length === 1 ?
+								  'Video awaiting approval' :
+								  'Videos awaiting approval'
+					callback(null, [grammar, query.length])
+				})
+			},
+			function(callback) {
+				callback(null, 15)
 			}
-		}
-	})
+		],
+		function(err, callback) {
+			for (var i in callback) {
+				if (typeof(callback[i]) === 'object') { // Grammar easter egg
+					data['boxes'][i].title = callback[i][0]
+					data['boxes'][i].data = callback[i][1]
+				} else {
+					data['boxes'][i].data = callback[i]
+					if (i == callback.length - 1) res.render('admin/admin', data)				
+				}
+			}
+		})
 }
 
 
 module.exports.getEpisodes = function(req, res) {
 	sequelize.query('SELECT * FROM Episodes WHERE approved = 1').success(function(query) {
-		if (query.length > 0) {
-			var data = {
-				videos: []
-			};
-			data['videos'] = query;
-			for (var i=0;i<data['videos'].length;i++) {
-				var element = data['videos'][i]
-				var eId = element.id
-				sequelize.query('SELECT * FROM Shownotes WHERE EpisodeId = ? LIMIT 1', null, {raw: true}, [eId]).success(function(shownotes) {
-					shownotes[0].content = shownotes[0].content.toString()
-					shownotes[0].shortened = shownotes[0].content.replace(/(([^\s]+\s\s*){30})(.*)/,"$1…")
-					if (shownotes) {
-						element.shownotes = shownotes
-					} else {
-						element.shownotes = null
-					}
-					console.log(element)
-					res.render('admin-episodes', data)
-				})
+			if (query.length > 0) {
+				var data = {
+					videos: []
+				};
+				data['videos'] = query;
+				for (var i=0;i<data['videos'].length;i++) {
+					var element = data['videos'][i]
+					var eId = element.id
+					sequelize.query('SELECT * FROM Shownotes WHERE EpisodeId = ? LIMIT 1', null, {raw: true}, [eId]).success(function(shownotes) {
+						shownotes[0].content = shownotes[0].content.toString()
+						shownotes[0].shortened = shownotes[0].content.replace(/(([^\s]+\s\s*){30})(.*)/,"$1…")
+						if (shownotes) {
+							element.shownotes = shownotes
+						} else {
+							element.shownotes = null
+						}
+						console.log(element)
+						res.render('admin/admin-episodes', data)
+					})
+				}
+			} else {
+				res.render('admin/admin-episodes')
 			}
-		} else {
-			res.render('admin/admin-episodes')
-		}
-	})
+		})
 }
 
 module.exports.getEpisodeById = function(req, res) {
@@ -130,6 +130,26 @@ module.exports.getUsers = function(req, res) {
 
 module.exports.getUserById = function(req, res) {
 	res.render('admin/admin')
+}
+
+module.exports.postApproveScreencast = function(req, res) {
+	if (req.xhr) {
+		sequelize.query('UPDATE Episodes SET approved = 1 WHERE id = :id', null, {raw: true}, {id: req.body.id}).success(function(approved) {
+			var successJson = {
+				status: 'ok',
+				rowsModified: 1
+			}
+			res.write(JSON.stringify(successJson))
+			res.end()
+		}).error(function(error) {
+			var errorJson = {
+				status: 'error',
+				rowsModified: null
+			}
+			res.write(errorJson)
+			res.end()
+		})
+	}
 }
 
 function requireViewer(req, res, next) {
