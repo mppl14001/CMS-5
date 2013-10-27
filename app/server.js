1ae39794684f61e790b28bcf347755b7f2cfc9b3
Charley Hutchison <glenwayguy@gmail.com>
2013-10-27 14:21:40 -0500

    Added controller files to seperate MVC.

--- app/server.js
+++ app/server.js
@@ -22,6 +22,10 @@ var User = models.user
 // DB
 var sequelize = new Sequelize(dbConfig.name, dbConfig.user, dbConfig.password)
 
+var adminController = require('./controllers/admin.js')
+console.log(adminController)
+var episodeController = require('./controllers/episode.js')
+
 // Passport
 var TwitterStrategy = passportTwitter.Strategy
 passport.serializeUser(function(user, done) {
@@ -84,187 +88,24 @@ app.get('/logout', function(req, res) {
 	res.redirect('/')
 })
 
-app.get('/:id(\\d+)', function(req, res) {
+app.get('/:id(\\d+)', episodeController.getEpisodeById)
 
-	var episodeNumber = parseInt(req.param('id'), 10)
+app.get('/admin',/*requireAdmin,*/ adminController.get)
 
-	if (episodeNumber) {
+app.get('/admin/episodes',/*requireAdmin,*/ adminController.getEpisodes)
 
-		Episode.find(episodeNumber).success(function(episode) {
+app.get('/admin/episodes/pending',/*requireAdmin,*/ adminController.getPendingEpisodes)
 
-			if (episode) {
-				// Return episode
-				res.end()
-			} else {
-				res.send(404, 'Episode not found.')
-			}
-		})
-	}
-})
+app.get('/admin/episodes/pending/:id(\\d+)',/*requireAdming,*/ adminController.getPendingEpisodeById)
 
-app.get('/admin', /*requireAdmin,*/ function(req, res) {
-	var data = {
-		boxes: [
-			{
-				title: "Video views today",
-				data: 0
-			},
-			{
-				title: "Videos awaiting approval",
-				data: 0
-			},
-			{
-				title: "Transcriptions awaiting approval",
-				data: 0
-			},
-			{
-				title: "Some title here",
-				data: "Data"
-			},
-			{
-				title: "Some title here",
-				data: "Data"
-			},
-			{
-				title: "Some title here",
-				data: "Data"
-			}
-		]
-	}
-	async.parallel([
-		function(callback) { // Total views
-			callback(null, '12,428')
-		}, 
-		function(callback) { // Videos awaiting approval
-			sequelize.query('SELECT * FROM Episodes WHERE approved = 0').success(function(query) {
-				var grammar = query.length === 1 ?
-							  'Video awaiting approval' :
-							  'Videos awaiting approval'
-				callback(null, [grammar, query.length])
-			})
-		},
-		function(callback) {
-			callback(null, 15)
-		}
-	],
-	function(err, callback) {
-		for (var i in callback) {
-			if (typeof(callback[i]) === 'object') { // Grammar easter egg
-				data['boxes'][i].title = callback[i][0]
-				data['boxes'][i].data = callback[i][1]
-			} else {
-				data['boxes'][i].data = callback[i]
-				if (i == callback.length - 1) res.render('admin/admin', data)				
-			}
-		}
-	})
-})
+app.get('/admin/episodes/:id(\\d+)',/*requireAdmin,*/ adminController.getEpisodeById)
 
-app.get('/admin/episodes', /*requireAdmin,*/ function(req, res) {
-	sequelize.query('SELECT * FROM Episodes WHERE approved = 1').success(function(query) {
-		if (query.length > 0) {
-			var data = {
-				videos: []
-			};
-			data['videos'] = query;
-			for (var i=0;i<data['videos'].length;i++) {
-				var element = data['videos'][i]
-				var eId = element.id
-				sequelize.query('SELECT * FROM Shownotes WHERE EpisodeId = ? LIMIT 1', null, {raw: true}, [eId]).success(function(shownotes) {
-					shownotes[0].content = shownotes[0].content.toString()
-					shownotes[0].shortened = shownotes[0].content.replace(/(([^\s]+\s\s*){30})(.*)/,"$1…")
-					if (shownotes) {
-						element.shownotes = shownotes
-					} else {
-						element.shownotes = null
-					}
-					console.log(element)
-					res.render('admin-episodes', data)
-				})
-			}
-		} else {
-			res.render('admin/admin-episodes')
-		}
-	})
-})
+app.get('/admin/users',/*requireAdmin,*/ adminController.getUsers)
 
-app.get('/admin/episodes/pending', /*requireAdmin,*/ function(req, res) {
-	sequelize.query('SELECT * FROM Episodes WHERE approved = 0').success(function(query) {
-		if (query.length > 0) {
-			var data = {
-				videos: []
-			}
-			data['videos'] = query;
-			for (var i=0;i<data['videos'].length;i++) {
-				var element = data['videos'][i]
-				var eId = element.id
-				sequelize.query('SELECT * FROM Shownotes WHERE EpisodeId = ? LIMIT 1', null, {raw: true}, [eId]).success(function(shownotes) {
-					shownotes[0].content = shownotes[0].content.toString()
-					shownotes[0].shortened = shownotes[0].content.replace(/(([^\s]+\s\s*){30})(.*)/,"$1…")
-					if (shownotes) {
-						element.shownotes = shownotes
-					} else {
-						element.shownotes = null
-					}
-					console.log(element)
-					res.render('admin/admin-episodes-pending', data)
-				})
-			}
-		} else {
-			res.render('admin/admin-episodes-pending')
-		}
-	})
-})
-
-app.get('/admin/episodes/pending/:id(\\d+)', /*requireAdming,*/ function(req, res) {
-	
-	res.render('admin/admin-episodes-specific')
-})
-
-app.get('/admin/episodes/:id(\\d+)', /*requireAdmin,*/ function(req, res) {
-	res.render('admin/admin-episodes-specific')
-})
-
-app.get('/admin/users', /*requireAdmin,*/ function(req, res) {
-	res.render('admin/admin-users')
-})
-
-app.get('/admin/users/:id(\\d+)', /*requireAdmin,*/ function(req, res) {
-	res.render('admin/admin')
-})
+app.get('/admin/users/:id(\\d+)',/*requireAdmin,*/ adminController.getUserById)
 
 app.listen(config.get('port') || 3000)
 
 // Passport roles
 
-function requireViewer(req, res, next) {
-	if (req.user && req.user.role === 4) {
-		next()
-	} else {
-		res.redirect('/')
-	}
-}
 
-function requireModerator(req, res, next) {
-	if (req.user && (req.user.role === 3 || req.user.role === 1)) {
-		next()
-	} else {
-		res.redirect('/')
-	}
-}
-
-function requireScreencaster(req, res, next) {
-	if (req.user && (req.user.role === 2 || req.user.role === 1)) {
-		next()
-	} else {
-		res.redirect('/')
-	}
-}
-
-function requireAdmin(req, res, next) {
-	if (req.user && req.user.role === 1) {
-		next()
-	} else {
-		res.redirect('/')
-	}
-}
