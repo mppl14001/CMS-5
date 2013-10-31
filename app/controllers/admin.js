e04b5ab73d9afe087abe6bd509314601259a2fe0
Joe Torraca <joe@joetorraca.com>
2013-10-31 19:04:14 -0400

    Added activeTranscription API

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -351,7 +351,22 @@ module.exports.removeTranscription = function(req, res) {
 
 module.exports.activateTranscription = function(req, res) {
 	if (req.xhr) {
-
+		sequelize.query('UPDATE Transcriptions SET approved = 1 WHERE id = :id AND EpisodeId = :eId', null, {raw: true}, {id: req.body.id, eId: req.body.episode}).success(function(query) {
+			var json = {
+				status: "ok",
+				rowsModified: 1
+			}
+			res.write(JSON.stringify(json))
+			res.end()
+		}).error(function(error) {
+			var json = {
+				status: "error",
+				rowsModified: null,
+				error: "sequelize"
+			}
+			res.write(JSON.stringify(json))
+			res.end()
+		})
 	}
 }
 
