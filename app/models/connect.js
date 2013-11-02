b4b3da21e1b80e06e12857a5dd6cb6403e98c2df
Ross Penman <ross@pen.mn>
2013-11-02 21:31:26 +0000

    Removed (apparently) unnecessary callback.

--- app/models/connect.js
+++ app/models/connect.js
@@ -2,9 +2,4 @@ var mongoose = require('mongoose')
 var db = mongoose.connection
 
 db.on('error', console.error)
-
-module.exports = function(next) {
-  db.once('open', next)
-}
-
 mongoose.connect('mongodb://localhost/codepilot-dev')
--- app/models/index.js
+++ app/models/index.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose')
 
-require("./connect")(function() {
-	GLOBAL.User = require(__dirname + '/user.js')
-	GLOBAL.Episode = require(__dirname + '/episode.js')
-	GLOBAL.Shownotes = require(__dirname + '/shownotes.js')
-	GLOBAL.Tag = require(__dirname + '/tag.js')
-	GLOBAL.Transcription = require(__dirname + '/transcription.js')
-})
+// connect
+require("./connect")
 
+GLOBAL.User = require(__dirname + '/user.js')
+GLOBAL.Episode = require(__dirname + '/episode.js')
+GLOBAL.Shownotes = require(__dirname + '/shownotes.js')
+GLOBAL.Tag = require(__dirname + '/tag.js')
+GLOBAL.Transcription = require(__dirname + '/transcription.js')
