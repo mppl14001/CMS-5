d75eb7b617db2cbaddf191ee81981c03594f75b1
Will Smidlein <Will@WillSmidlein.com>
2013-11-02 17:03:52 -0400

    Fixed it

--- app/models/index.js
+++ app/models/index.js
@@ -1,7 +1,5 @@
 var mongoose = require('mongoose')
 
-var db = mongoose.connection
-
 var EpisodeSchema = new mongoose.Schema({
 	title: {type: String},
 	ytURL: {type: String},
@@ -23,9 +21,4 @@ var EpisodeSchema = new mongoose.Schema({
 
 var EpisodeModel = mongoose.model('Episode', EpisodeSchema)
 
-db.on('error', console.error)
-db.once('open', function() {
-
-})
-
-mongoose.connect('mongodb://localhost/codepilot-dev')
+module.exports.Episode = EpisodeModel
