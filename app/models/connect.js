dbe5bfa465325509b537723b24de5231740e4b93
Ross Penman <ross@pen.mn>
2013-11-02 21:28:08 +0000

    Removed a stupid function.

--- app/models/connect.js
+++ app/models/connect.js
@@ -4,9 +4,7 @@ var db = mongoose.connection
 db.on('error', console.error)
 
 module.exports = function(next) {
-  db.once('open', function() {
-    next()
-  })
+  db.once('open', next)
 }
 
 mongoose.connect('mongodb://localhost/codepilot-dev')
