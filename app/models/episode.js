8ff79eb0ce82fa5988c4b504a7aedd868717045b
Lenny Khazan <lenny.khazan@gmail.com>
2013-11-02 17:15:25 -0400

    Moved episode schema

---
+++ app/models/episode.js
@@ -0,0 +1,22 @@
+var EpisodeSchema = new mongoose.Schema({
+	title: {type: String},
+	ytURL: {type: String},
+	published: {type: Boolean},
+	approved: {type: Boolean},
+	tags: [{
+		text: String
+	}],
+	shownotes: [{
+		text: Buffer,
+		language: String
+	}],
+	transcriptions: [{
+		approved: Boolean,
+		text: Buffer,
+		language: String
+	}]
+})
+
+var EpisodeModel = mongoose.model('Episode', EpisodeSchema)
+
+module.exports = EpisodeModel
--- app/models/index.js
+++ app/models/index.js
@@ -1,24 +1,4 @@
 var mongoose = require('mongoose')
 
-var EpisodeSchema = new mongoose.Schema({
-	title: {type: String},
-	ytURL: {type: String},
-	published: {type: Boolean},
-	approved: {type: Boolean},
-	tags: [{
-		text: String
-	}],
-	shownotes: [{
-		text: Buffer,
-		language: String
-	}],
-	transcriptions: [{
-		approved: Boolean,
-		text: Buffer,
-		language: String
-	}]
-})
-
-var EpisodeModel = mongoose.model('Episode', EpisodeSchema)
-
-module.exports.Episode
+var User = require(__dirname + '/user.js')
+var Episode = require(__dirname + '/episode.js')
