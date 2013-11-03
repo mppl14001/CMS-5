6eb6053be860b170c732fd397ef206df4f918407
Lenny Khazan <lenny.khazan@gmail.com>
2013-11-02 23:14:07 -0400

    Added creator reference

--- app/models/episode.js
+++ app/models/episode.js
@@ -17,7 +17,8 @@ var EpisodeSchema = new mongoose.Schema({
 		approved: Boolean,
 		text: Buffer,
 		language: String
-	}]
+	}],
+	creator: {type: mongoose.Schema.Types.ObjectId}
 })
 
 var EpisodeModel = mongoose.model('Episode', EpisodeSchema)
