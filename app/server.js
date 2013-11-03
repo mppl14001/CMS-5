02ec68cea506285647397abb90f876bfbba52a2d
Will Smidlein <Will@WillSmidlein.com>
2013-11-03 10:46:54 -0500

    More deletions (surprise surprise)

--- app/server.js
+++ app/server.js
@@ -1,47 +1,18 @@
 // Node Module Requirements
-var nconf = require('nconf')
+var nconf = 
 var path = require('path')
 var passport = require('passport')
 var passportTwitter = require('passport-twitter')
 GLOBAL._ = require('lodash')
 
 // Config
-GLOBAL.config = nconf.argv()
-					 .env()
-					 .file({ file: path.join(__dirname, 'config.json') })
-var twitterConfig = config.get('twitter')
-
+GLOBAL.config = require('nconf').argv().env().file({ file: path.join(__dirname, 'config.json') })					 
 
 // Models
 GLOBAL.models = require('./models')
 
 // Fixtures
-if (config.get('seed-data')) {
-	var fixtures = require('./misc/fixtures.js')
-	fixtures()
-}
-
-// WILL'S DEBUG CODE
-
-// var test = new models.Episode({
-// 	id: 0,
-// 	title: 'Hello, world',
-// 	ytURL: 'http://google.com',
-// 	published: false,
-// 	approved: false,
-// 	shownotes: [
-// 		{
-// 			text: 'ABCDEF',
-// 			language: 'en'
-// 		},
-// 		{
-// 			text: 'YOLOSWAG',
-// 			language: 'es'
-// 		},
-// 	]
-// })
-
-// test.save()
+if (config.get('seed-data')) { require('./misc/fixtures.js')() }
 
 // Controllers
 var adminController = require('./controllers/admin.js')
@@ -53,7 +24,7 @@ var searchController = require('./controllers/search.js')
 // Express
 var app = require("./app")
 
-var passport = require('./passport')(app, twitterConfig.key, twitterConfig.secret)
+var passport = require('./passport')(app, config.get('twitter').key, config.get('twitter').secret)
 
 app.use(function(req, res, next) {
 	res.locals.user = req.user
