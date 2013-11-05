a470e59ee7954e337a04ca7064c1db45a25f663e
Lenny Khazan <futurevilla216@aim.com>
2013-11-05 09:50:55 -0500

    Organized index.js

--- app/models/index.js
+++ app/models/index.js
@@ -2,18 +2,25 @@ var mongoose = require('mongoose')
 
 var db = require('./connect')
 
+// Plugins
+
 var autoIncrement = require('mongoose-auto-increment')
 autoIncrement.initialize(db)
 
 var mongoosastic = require('mongoosastic')
 
+// Schemas
+
 var UserSchema = require(__dirname + '/user.js')
 var EpisodeSchema = require(__dirname + '/episode.js')
 var TranscriptionSchema = require(__dirname + '/transcription.js')
 
+// Initialize Plugins
+
+EpisodeSchema.plugin(mongoosastic)
+
 UserSchema.plugin(autoIncrement.plugin, {field: 'id'})
 EpisodeSchema.plugin(autoIncrement.plugin, {field: 'id'})
-EpisodeSchema.plugin(mongoosastic)
 TranscriptionSchema.plugin(autoIncrement.plugin, {field: 'id'})
 
 module.exports.User = db.model('User', UserSchema)
