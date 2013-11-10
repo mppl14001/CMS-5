46c2278d4c1ed7a30952141cf41d03d3c575527b
Joe Torraca <joe@joetorraca.com>
2013-11-10 12:07:44 -0500

    Removed useless comment

--- app/views/admin/helpers/userRoleHelper.js
+++ app/views/admin/helpers/userRoleHelper.js
@@ -3,7 +3,6 @@ function roleToString(role){
 		case 1:	return 'Admin'
 		case 2:	return 'Screencaster'
 		case 3:	return 'Moderator'
-		// 4 should be viewer, so just let it hit default.
 		default: return 'Viewer'
 	}	
 }
